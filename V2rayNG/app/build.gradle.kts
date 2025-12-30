94f0a25de2ad998f3f0866879940ab6ed6b42c34
TheFachur <future.bitter.85@gmail.com>
2025-12-31 02:47:41 +0330

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = false // غیرفعال کردن کوچک‌سازی برای جلوگیری از خطا
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -58,6 +58,7 @@ android {
 
     sourceSets {
         getByName("main") {
+            // این خط به برنامه می‌گوید فایل‌های .so کپی شده در libs را بخواند
             jniLibs.srcDirs("libs")
         }
     }
@@ -93,7 +94,9 @@ android {
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar", "*.jar"))))
+    // بارگذاری فایل‌های aar و jar از پوشه libs
+    implementation(fileTree(mapOf("dir" to project.file("libs"), "include" to listOf("*.aar", "*.jar"))))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
